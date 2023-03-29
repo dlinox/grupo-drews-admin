f1186fb4f093494f45bad4476e03572b821324d2
Lino Puma Ticona <nearlino20@gmail.com>
2023-03-28 19:53:44 -0500

    forece 2

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,12 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <h1>You are currently not connected to any networks.</h1>
-
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    No tienes internet
+</body>
+</html>
